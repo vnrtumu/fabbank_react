15c2a3d4db1a8215a95a831c304c74191bb64c72
apple <vnr.tumu@gmail.com>
2020-01-30 16:00:37 +0530

    test

---
+++ build/precache-manifest.f225db9d8c01c90695c137009f591f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f07b91e4b1a5f58f7ff9da9c792db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "198eea3cc17913aebb63",
+    "url": "/static/css/main.92422bd1.chunk.css"
+  },
+  {
+    "revision": "03a51f4a13225afcf1d1",
+    "url": "/static/js/2.cae2c8c4.chunk.js"
+  },
+  {
+    "revision": "737e232b3a800318023ca90f91a78fce",
+    "url": "/static/js/2.cae2c8c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "198eea3cc17913aebb63",
+    "url": "/static/js/main.d56010ef.chunk.js"
+  },
+  {
+    "revision": "1d2c9fd8b226adb4086c",
+    "url": "/static/js/runtime-main.ff0eb6da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
